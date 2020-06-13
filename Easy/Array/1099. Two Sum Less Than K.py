e0805739bf596aedf50ecf8dc15afe52b9a40d84
Yunting Chiu <58647044+twyunting@users.noreply.github.com>
2020-06-13 18:49:47 +0800

    Update 1099. Two Sum Less Than K.py

--- Easy/Array/1099. Two Sum Less Than K.py
+++ Easy/Array/1099. Two Sum Less Than K.py
@@ -12,3 +12,23 @@ class Solution:
    
    
    
+# Two pointer solution
+
+class Solution:
+    def twoSumLessThanK(self, A: List[int], K: int) -> int:
+        
+        A.sort()
+        result = -1
+        left, right = 0, len(A)-1
+        
+        
+        while left < right:
+            if A[left] + A[right] < K:
+                result = max(result, A[left] + A[right])
+                left += 1
+            else:
+                right -= 1
+        
+        return result
+    
+    
