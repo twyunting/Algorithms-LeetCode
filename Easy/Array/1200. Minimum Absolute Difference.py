71b9e23289f25bb211aa3d3f5e784edc7bbb2bf5
Yunting Chiu <58647044+twyunting@users.noreply.github.com>
2020-06-22 00:02:57 +0800

    Create 1200. Minimum Absolute Difference.py

---
+++ Easy/Array/1200. Minimum Absolute Difference.py
@@ -0,0 +1,37 @@
+class Solution:
+    def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
+        
+        arr.sort()
+        minDiff = float("inf")
+        minList = []
+        
+        for index in range(len(arr) - 1):
+            minDiff = min(minDiff, abs(arr[index+1] - arr[index]))
+            
+        for index in range(len(arr) - 1):
+            if abs(arr[index+1] - arr[index]) == minDiff:
+                minList.append([arr[index],arr[index + 1]])
+                
+        return minList
+        
+        
+       
+    # Dicttionary Method
+        
+        class Solution:
+    def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
+        
+        dic = {}
+        arr.sort()
+        
+        for i in range(len(arr)-1):
+            d = arr[i+1] - arr[i]
+            
+            if d not in dic:
+                dic[d] = []
+            
+            dic[d].append([arr[i], arr[i+1]])
+        
+        return dic[min(dic)]
+        
+        
