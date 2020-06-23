aa469bb1f45e054f9a4339052c0fe4c150d30c44
Yunting Chiu <58647044+twyunting@users.noreply.github.com>
2020-06-23 08:51:27 +0800

    Create 1217. Play with Chips.py

---
+++ Easy/Array/1217. Play with Chips.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minCostToMoveChips(self, chips: List[int]) -> int:
+        
+        odd_parity, even_parity = 0, 0
+        
+        for i in chips:
+            if i % 2 == 0:
+                even_parity += 1
+            else:
+                odd_parity += 1
+            
+        return min(even_parity, odd_parity)
+
