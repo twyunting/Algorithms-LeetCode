ade5c3269782f36b9f944eacd2d64288c09a0f39
Yunting Chiu <58647044+twyunting@users.noreply.github.com>
2020-07-11 19:18:28 -0400

    Create 1304. Find N Unique Integers Sum up to Zero.py

---
+++ Easy/Array/1304. Find N Unique Integers Sum up to Zero.py
@@ -0,0 +1,15 @@
+class Solution:
+    def sumZero(self, n: int) -> List[int]:
+        
+        arr = []
+        for i in range(1, int(n/2)+1):
+            arr.append(i)
+            arr.append(-i)
+        
+        if n % 2 != 0:
+            arr.append(0)
+            
+        return arr
+        
+        
+        
