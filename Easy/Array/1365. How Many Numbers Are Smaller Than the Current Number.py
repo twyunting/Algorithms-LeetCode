ce8d92ff2c9dc5f69cbb630ece1d178b74677ebd
Yunting Chiu <58647044+twyunting@users.noreply.github.com>
2020-07-16 09:25:40 -0400

    Update 1365. How Many Numbers Are Smaller Than the Current Number.py

--- Easy/Array/1365. How Many Numbers Are Smaller Than the Current Number.py
+++ Easy/Array/1365. How Many Numbers Are Smaller Than the Current Number.py
@@ -10,4 +10,21 @@ class Solution:
         return arr
             
             
+
+# Brute Force Method
+
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+    
+        arr = []
+        
+        for i in range(len(nums)):
+            count = 0
+            for j in range(len(nums)):
+                if i != j and nums[i] > nums[j]:
+                    count += 1
+            arr.append(count)
+        return arr
             
+        
+        
