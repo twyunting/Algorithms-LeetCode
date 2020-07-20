a2d81eccb98e3f7a2bb77d8bb3f309a05d6fdb27
Yunting Chiu <58647044+twyunting@users.noreply.github.com>
2020-07-19 20:27:34 -0400

    Update 1413. Minimum Value to Get Positive Step by Step Sum.py

--- Easy/Array/1413. Minimum Value to Get Positive Step by Step Sum.py
+++ Easy/Array/1413. Minimum Value to Get Positive Step by Step Sum.py
@@ -2,7 +2,7 @@ class Solution:
     def minStartValue(self, nums: List[int]) -> int:
         
         for i in range(1, len(nums)):
-            nums[i] = nums[i] + nums[i-1]
+            nums[i] = nums[i] + nums[i-1]  # prefixed sum
             
         if min(nums) <= 0:
             return -min(nums) + 1
