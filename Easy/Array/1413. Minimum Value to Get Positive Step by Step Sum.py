a819a77bb9f56bfc4bfc392ea6b2248c0c9219c8
Yunting Chiu <58647044+twyunting@users.noreply.github.com>
2020-07-19 20:25:38 -0400

    Create 1413. Minimum Value to Get Positive Step by Step Sum.py

---
+++ Easy/Array/1413. Minimum Value to Get Positive Step by Step Sum.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minStartValue(self, nums: List[int]) -> int:
+        
+        for i in range(1, len(nums)):
+            nums[i] = nums[i] + nums[i-1]
+            
+        if min(nums) <= 0:
+            return -min(nums) + 1
+        else:
+            return 1
+            
+            
+# Second
+
+class Solution:
+    def minStartValue(self, nums: List[int]) -> int:
+        
+        prefixSum = 0
+        minSunValue = 1
+        
+        for i in nums:
+            prefixSum += i
+            minSunValue = max(minSunValue, 1-prefixSum)
+        
+        return minSunValue
