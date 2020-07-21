be4b1379afc56e7150a2bf2e3982d53b6dd65a88
Yunting Chiu <58647044+twyunting@users.noreply.github.com>
2020-07-21 17:43:39 -0400

    Update 1427. Perform String Shifts.py

--- Easy/Array/1427. Perform String Shifts.py
+++ Easy/Array/1427. Perform String Shifts.py
@@ -11,3 +11,17 @@ class Solution:
         return s
         
         
+# Second Method
+
+class Solution:
+    def stringShift(self, s: str, shift: List[List[int]]) -> str:
+        move = 0
+        for x, y in shift:
+            if x == 0:
+                move -= y
+            else:
+                move += y
+        move %= len(s)
+        return s[-move:] + s[:-move]
+    
+    
