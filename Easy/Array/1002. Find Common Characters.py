e3414df0420f3999e0771c48d7996e89d1847965
Yunting Chiu <58647044+twyunting@users.noreply.github.com>
2020-07-21 19:10:41 -0400

    Update 1002. Find Common Characters.py

--- Easy/Array/1002. Find Common Characters.py
+++ Easy/Array/1002. Find Common Characters.py
@@ -10,6 +10,12 @@ class Solution:
         return list(text.elements())
     
     # https://docs.python.org/3/library/collections.html
+    """
+    Example:
     
+    c = Counter(a=4, b=2, c=0, d=-2)
+    sorted(c.elements())
+    ['a', 'a', 'a', 'a', 'b', 'b']
     
+   """ 
     
