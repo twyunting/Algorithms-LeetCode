0e8043676f875593e0fff452363153e685fb8af3
Yunting Chiu <58647044+twyunting@users.noreply.github.com>
2020-07-24 14:59:42 -0400

    Update 1491. Average Salary Excluding the Minimum and Maximum Salary.py

--- Easy/Array/1491. Average Salary Excluding the Minimum and Maximum Salary.py
+++ Easy/Array/1491. Average Salary Excluding the Minimum and Maximum Salary.py
@@ -6,3 +6,11 @@ class Solution:
         return sum(salarySort) / (len(salary)-2)
         
         
+# Straightforward
+
+class Solution:
+    def average(self, salary: List[int]) -> float: 
+        
+        return (sum(salary)-max(salary)-min(salary)) / (len(salary)-2)
+    
+    
