ef8ba3e2166ee18c3d579a53adefac9379541bf0
Yunting Chiu <58647044+twyunting@users.noreply.github.com>
2020-07-24 13:39:26 -0400

    Update 1480. Running Sum of 1d Array.py

--- Easy/Array/1480. Running Sum of 1d Array.py
+++ Easy/Array/1480. Running Sum of 1d Array.py
@@ -8,5 +8,26 @@ class Solution:
             ttl += i
             res.append(ttl)
         return res
-            
-            
+   
+
+# Traversal Method
+
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        
+        res = []
+        
+        res.append(nums[0])
+        for i in range(1,len(nums)):
+            res.append(res[i-1] + nums[i])
+        return res   
+    
+
+# itertools.accumulate
+
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        
+        return itertools.accumulate(nums)
+    
+    
