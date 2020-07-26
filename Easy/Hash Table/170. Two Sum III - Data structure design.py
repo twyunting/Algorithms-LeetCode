8edb3bd2271bfcda5391af6c7355aa8b5b4045a7
Yunting Chiu <58647044+twyunting@users.noreply.github.com>
2020-07-26 12:33:42 -0400

    Update 170. Two Sum III - Data structure design.py

--- Easy/Hash Table/170. Two Sum III - Data structure design.py
+++ Easy/Hash Table/170. Two Sum III - Data structure design.py
@@ -35,5 +35,39 @@ class TwoSum:
 # param_2 = obj.find(value)
 
 
+# Second Method
+
+class TwoSum:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.arr = []
+
+    def add(self, number: int) -> None:
+        """
+        Add the number to an internal data structure..
+        """
+        self.arr.append(number)
+
+    def find(self, value: int) -> bool:
+        """
+        Find if there exists any pair of numbers which sum is equal to the value.
+        """
+        d = {}
+        for i in self.arr:
+            if i in d:
+                return True
+            else:
+                d[value-i] = i
+        return False
+
+
+# Your TwoSum object will be instantiated and called as such:
+# obj = TwoSum()
+# obj.add(number)
+# param_2 = obj.find(value)
+
 
 
