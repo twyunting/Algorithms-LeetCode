d346904c03d6262b65ff590453feb35850d83115
Yunting Chiu <58647044+twyunting@users.noreply.github.com>
2020-07-27 14:51:28 -0400

    Update 202. Happy Number.py

--- Easy/Hash Table/202. Happy Number.py
+++ Easy/Hash Table/202. Happy Number.py
@@ -6,4 +6,21 @@ class Solution:
             n = sum(int(i) **2 for i in str(n))
         return n == 1 
         
+
+# My method
+
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        s = set()
+        while n != 1:
+            if n in s:
+                return False
+            s.add(n)
+            total = 0
+            for i in str(n):
+                total += pow(int(i),2)
+            n = total
+        return True
+    
+    
         
