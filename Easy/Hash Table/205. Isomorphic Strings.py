18b50262c8bda3548d19adee6091194e59d61ca1
Yunting Chiu <58647044+twyunting@users.noreply.github.com>
2020-08-01 12:59:21 -0400

    Create 205. Isomorphic Strings.py

---
+++ Easy/Hash Table/205. Isomorphic Strings.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        
+        if len(s) != len(t):
+            return False
+        return self.isIso(s, t) and self.isIso(t, s)
+    
+    def isIso(self,s ,t ):
+        d = {}
+        for i in range(len(s)):
+            if s[i] not in d:
+                d[s[i]] = t[i]
+            elif d[s[i]] != t[i]:
+                return False
+        return True
+        
+        
+        
