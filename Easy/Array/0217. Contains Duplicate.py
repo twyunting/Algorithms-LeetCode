866be972e84bb7e255584cc7c62b677e1965c12e
Yunting Chiu <58647044+twyunting@users.noreply.github.com>
2020-08-02 10:46:38 -0400

    Update 0217. Contains Duplicate.py

--- Easy/Array/0217. Contains Duplicate.py
+++ Easy/Array/0217. Contains Duplicate.py
@@ -6,3 +6,16 @@ class Solution:
                 return True
         return False
         
+# Counter
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+    
+        s = collections.Counter(nums)
+        
+        for i in s.values():
+            if i >= 2:
+                return True
+        return False
+    
+    
