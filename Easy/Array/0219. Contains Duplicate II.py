92ffe5df4db3611c79c4d662e357846cf7167c6f
Yunting Chiu <58647044+twyunting@users.noreply.github.com>
2020-08-03 11:01:48 -0400

    Update 0219. Contains Duplicate II.py

--- Easy/Array/0219. Contains Duplicate II.py
+++ Easy/Array/0219. Contains Duplicate II.py
@@ -35,18 +35,20 @@ class Solution:
 返回False
 
 """
-
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        hash={}
+        
+        d = {}
         for i in range(len(nums)):
-            if(nums[i] not in hash):
-                hash[nums[i]]=i
+            if nums[i] not in d:
+                d[nums[i]] = i
             else:
-                if(i-hash[nums[i]]<=k):
+                if i-d[nums[i]] <= k:
                     return True
                 else:
-                    hash[nums[i]]=i
+                    d[nums[i]] = i
         return False
+    
+    
     
     
