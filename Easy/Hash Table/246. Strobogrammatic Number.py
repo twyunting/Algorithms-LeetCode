e5682f0ba5f28d76aa760cccb3086b383c5a628f
Yunting Chiu <58647044+twyunting@users.noreply.github.com>
2020-08-03 11:37:00 -0400

    Update 246. Strobogrammatic Number.py

--- Easy/Hash Table/246. Strobogrammatic Number.py
+++ Easy/Hash Table/246. Strobogrammatic Number.py
@@ -10,4 +10,22 @@ class Solution:
                 return False
         return True
         
+# two pointers
+
+class Solution:
+    def isStrobogrammatic(self, num):
+    
+        d = {"0":"0", "1":"1", "6":"9", "8":"8", "9":"6"}
+        
+        start, end = 0, len(num)-1 # end is index's position
+        while start <= end:
+            if num[start] not in d:
+                return False
+            if d[num[start]] != num[end]:
+                return False    
+            start += 1
+            end -= 1
+        return True
+        
+        
         
