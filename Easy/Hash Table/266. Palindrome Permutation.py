948c023805feb04f2937345793ca4eb433f5a5a5
Yunting Chiu <58647044+twyunting@users.noreply.github.com>
2020-08-03 14:06:01 -0400

    Update 266. Palindrome Permutation.py

--- Easy/Hash Table/266. Palindrome Permutation.py
+++ Easy/Hash Table/266. Palindrome Permutation.py
@@ -22,3 +22,13 @@ class Solution:
         return sum( i % 2 != 0 for i in collections.Counter(s).values()) <= 1
     
     
+"""
+From looking at different types of strings, we can conclude:
+A string is a palindrome if and only if we have zero or at most one letter that occurs an odd number of times.
+Everything else cannot be a palindrome.
+
+Examples: aaacbbb (one letter occurs an odd number of times, thus, is a palindrome)
+aaa: one letter occurs an odd number of times, thus, is a palindrome
+aabb: zero letters occur an odd number of times, thus, is a palindrome
+aabbcd: two letters occur an odd number of times, thus, is not a palindrome
+"""
