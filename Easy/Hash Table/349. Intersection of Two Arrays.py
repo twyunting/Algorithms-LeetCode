3f01dca9dd35260e8c23e41865722d0832732a0b
Yunting Chiu <58647044+twyunting@users.noreply.github.com>
2020-08-06 15:24:47 -0400

    Update 349. Intersection of Two Arrays.py

--- Easy/Hash Table/349. Intersection of Two Arrays.py
+++ Easy/Hash Table/349. Intersection of Two Arrays.py
@@ -5,3 +5,15 @@ class Solution:
         
         return Counter(nums1) & Counter(nums2)
         
+
+# set
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        
+        set1 = set(nums1)
+        set2 = set(nums2)
+        
+        return nums1 & nums2
+    
+    
