9e3ab3f61b6f2ebad6c974c0bbd99758188977a2
Yunting Chiu <58647044+twyunting@users.noreply.github.com>
2020-08-06 15:45:13 -0400

    Update 349. Intersection of Two Arrays.py

--- Easy/Hash Table/349. Intersection of Two Arrays.py
+++ Easy/Hash Table/349. Intersection of Two Arrays.py
@@ -36,3 +36,23 @@ class Solution:
                     res.append(i)
         return res
     
+# two pointer
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1.sort()
+        nums2.sort()
+        ans=set()
+        i,j=0,0
+        while i<len(nums1) and j<len(nums2):
+            if nums1[i]==nums2[j]:
+                ans.add(nums1[i])
+                i+=1
+                j+=1
+            elif nums1[i]>nums2[j]:
+                j+=1
+            else:
+                i+=1
+        return list(ans)
+    
+    
