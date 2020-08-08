64411a81b7b4dbc407776b5fc2b240551aac9f65
Yunting Chiu <58647044+twyunting@users.noreply.github.com>
2020-08-08 12:08:46 -0400

    Update 350. Intersection of Two Arrays II.py

--- Easy/Hash Table/350. Intersection of Two Arrays II.py
+++ Easy/Hash Table/350. Intersection of Two Arrays II.py
@@ -41,5 +41,20 @@ class Solution:
             elif nums1[p1] < nums2[p2]:
                 p1 += 1
         return res
+
+# Counter
+    
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        
+        c1 = Counter(nums1)
+        c2 = Counter(nums2)
+        res = []
+        
+        for i in c1:
+            if i in c2:
+                res += [i] * min(c1[i], c2[i])
+        return res
+    
     
     
