a37bc71e51416daa192c1e9822d3100916d3b048
Yunting Chiu <58647044+twyunting@users.noreply.github.com>
2020-08-09 21:20:09 -0400

    Update 0409. Longest Palindrome Easy.py

--- Easy/Hash Table/0409. Longest Palindrome Easy.py
+++ Easy/Hash Table/0409. Longest Palindrome Easy.py
@@ -26,3 +26,17 @@ class Solution:
                 
         return one + count
         
+# offical answer
+
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        res = 0
+        count = Counter(s)
+        
+        for i in count.values():
+            res += i//2*2
+            if res % 2 == 0 and i % 2 == 1:
+                res += 1
+        return res
+    
+    
