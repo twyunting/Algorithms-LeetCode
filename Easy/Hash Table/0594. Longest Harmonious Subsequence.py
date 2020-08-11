604c68bd0af6d78c86f95f7cf963b1b4575ddfed
Yunting Chiu <58647044+twyunting@users.noreply.github.com>
2020-08-11 17:01:30 -0400

    Create 0594. Longest Harmonious Subsequence.py

---
+++ Easy/Hash Table/0594. Longest Harmonious Subsequence.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findLHS(self, nums: List[int]) -> int:
+        
+        d = Counter(nums)
+        res = 0
+        
+        for i, j in d.items():
+            if i+1 in d:
+                res = max(d[i]+d[i+1], res)
+        return res
+                
+                
