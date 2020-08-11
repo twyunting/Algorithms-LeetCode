28eca3be349d9291c708402f5fdae69feea90330
Yunting Chiu <58647044+twyunting@users.noreply.github.com>
2020-08-11 00:43:46 -0400

    Update 0463. Island Perimeter.py

--- Easy/Hash Table/0463. Island Perimeter.py
+++ Easy/Hash Table/0463. Island Perimeter.py
@@ -1,3 +1,35 @@
+# my solution
+
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        
+        result = 0
+        
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if grid[i][j] == 1:
+                    result += 4
+                    
+                    # i-1: check up
+                    if i > 0 and grid[i-1][j] == 1:
+                        result -= 1
+                        
+                    # i+1: check down
+                    if i < len(grid)-1 and grid[i+1][j] == 1:
+                        result -= 1
+                        
+                    # j-1: check left
+                    if j > 0 and grid[i][j-1] == 1: 
+                        result -= 1
+                    
+                    # j+1: check right
+                    if j < len(grid[i])-1 and grid[i][j+1] == 1: 
+                        result -= 1
+        
+        return result
+    
+# Leetcode offical answer
+
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
         
