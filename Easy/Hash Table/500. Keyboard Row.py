a2fbf9a685454ada1cb42c7d4a51caf3b6a0f83f
Yunting Chiu <58647044+twyunting@users.noreply.github.com>
2020-08-11 13:43:49 -0400

    Update 500. Keyboard Row.py

--- Easy/Hash Table/500. Keyboard Row.py
+++ Easy/Hash Table/500. Keyboard Row.py
@@ -16,4 +16,23 @@ class Solution:
                 res.append(word)
         return res
         
+ # same way
+
+class Solution:
+    def findWords(self, words: List[str]) -> List[str]:
         
+        row1 = set("QWERTYUIOPqwertyuiop")
+        row2 = set("ASDFGHJKLasdfghjkl")
+        row3 = set("ZXCVBNMzxcvbnm")
+        res = []
+        
+        for word in words:
+            i = set(word)
+            if i&row1 == i:
+                res.append(word)
+            elif i&row2 == i:
+                res.append(word)
+            elif i&row3 == i:
+                res.append(word)
+        return res
+    
