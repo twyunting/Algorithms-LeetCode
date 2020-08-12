096ae73aecc1a289aadd2fcbe5c9e3155c7e5ec3
Yunting Chiu <58647044+twyunting@users.noreply.github.com>
2020-08-12 15:33:26 -0400

    Update 0599. Minimum Index Sum of Two Lists.py

--- Easy/Hash Table/0599. Minimum Index Sum of Two Lists.py
+++ Easy/Hash Table/0599. Minimum Index Sum of Two Lists.py
@@ -1,3 +1,26 @@
+# Jerry's answer
+
+class Solution:
+    def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
+        
+        ans = []
+        first = ""
+        minimum = ("none",9999)
+        
+        for i, j in enumerate(list1):
+            if j not in list2:
+                continue
+            else:
+                if list2.index(j) + i < minimum[1]:
+                    first = j
+                    minimum = (j, list2.index(j)+i)
+                elif list2.index(j) + i == minimum[1]:
+                    ans.append(j)
+        ans.append(first)
+        
+        return ans
+    
+    
 # brute force with 3 dic
 
 class Solution:
