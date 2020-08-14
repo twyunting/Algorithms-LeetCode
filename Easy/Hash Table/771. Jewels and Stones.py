af24d3339d9f3ba0c1149327b68785d65731e2f1
Yunting Chiu <58647044+twyunting@users.noreply.github.com>
2020-08-14 14:35:58 -0400

    Update 771. Jewels and Stones.py

--- Easy/Hash Table/771. Jewels and Stones.py
+++ Easy/Hash Table/771. Jewels and Stones.py
@@ -15,4 +15,11 @@ class Solution:
                 count += 1
         return count
         
+# one line
+
+class Solution:
+    def numJewelsInStones(self, J: str, S: str) -> int:
         
+        return sum(i in J for i in S)
+    
+    
