a17cbd13c56be3ce26e800cc6e6f852b8e9abb54
Yunting Chiu <58647044+twyunting@users.noreply.github.com>
2020-08-17 19:12:07 -0400

    Update 1213. Intersection of Three Sorted Arrays.py

--- Easy/Hash Table/1213. Intersection of Three Sorted Arrays.py
+++ Easy/Hash Table/1213. Intersection of Three Sorted Arrays.py
@@ -13,7 +13,7 @@ class Solution:
         return final
         
         
-# Two pointers
+# Three pointers
 
 class Solution:
     def arraysIntersection(self, arr1: List[int], arr2: List[int], arr3: List[int]) -> List[int]:
