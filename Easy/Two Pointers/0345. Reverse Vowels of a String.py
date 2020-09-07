c823630a9872c8135523cb60f6fe9fc1a9d50027
twyunting <yc6705a@american.edu>
2020-09-07 01:09:55 -0400

    added set

---
+++ Easy/Two Pointers/0345. Reverse Vowels of a String.py
@@ -0,0 +1,13 @@
+def reverseVowels(s):
+		"""
+		:type s: str
+		:rtype: str
+		"""
+	i, j = 0, len(s) - 1
+	vowels = set("aeiouAEIOU")
+	
+	while i < j:
+		if i 
+s = "leetcode"
+
+print(reverseVowels(s))	
