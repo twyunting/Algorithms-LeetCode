e0d91fdf475195dedf4591cc842c1712da2676d9
twyunting <yc6705a@american.edu>
2020-09-15 02:54:19 -0400

    passed

--- Easy/Two Pointers/0844. Backspace String Compare.py
+++ Easy/Two Pointers/0844. Backspace String Compare.py
@@ -1,21 +1,14 @@
-def backspaceCompare(S, T):
-"""
-:type S: str
-:type T: str
-:rtype: bool
-"""
-	S, T = self.recheck(S), self.recheck(T)
-	return S == T
-
-	def recheck(strings):
-		stack = []
-		for i in nums:
-			if i != "#":
-				stack.append(i)
-			else: # if i == #
-				stack.pop(i)
-		return stack
-
-print(backspaceCompare(S = "ab#c", T = "ad#c"))
-print(backspaceCompare(S = "ab##", T = "c#d#"))
-print(backspaceCompare(S = "ab#c", T = "ad#c"))
+class Solution:
+    def backspaceCompare(self, S: str, T: str) -> bool:
+        S, T = self.recheck(S), self.recheck(T)
+        return S == T
+    
+    def recheck(self, strings):
+        stack = []
+        for i in strings:
+            if i != "#":
+                stack.append(i)
+            elif stack:
+                stack.pop()
+        return stack
+        
