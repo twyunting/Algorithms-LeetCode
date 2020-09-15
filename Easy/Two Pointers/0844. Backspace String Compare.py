b8a9aaddee1a2d6525d30301383207246a793312
twyunting <yc6705a@american.edu>
2020-09-15 03:09:39 -0400

    stack method

--- Easy/Two Pointers/0844. Backspace String Compare.py
+++ Easy/Two Pointers/0844. Backspace String Compare.py
@@ -1,3 +1,31 @@
+def backspaceCompare(S, T):
+	"""
+	:type S: str
+	:type T: str
+	:rtype: bool
+
+	"""
+	Sarr, Tarr = [], []
+	for i in S:
+		if i != "#":
+			Sarr.append(i)
+		elif len(Sarr) > 0: # make sure len(Sarr) not equal to 0
+			Sarr.pop()
+
+
+	for j in T:
+		if j != "#":
+			Tarr.append(j)
+		elif len(Tarr) > 0: # make sure len(Tarr) not equal to 0
+			Tarr.pop()
+
+	return Sarr == Tarr
+	
+print(backspaceCompare(S = "ab##", T = "c#d#"))
+
+
+
+"""
 class Solution:
     def backspaceCompare(self, S: str, T: str) -> bool:
         S, T = self.recheck(S), self.recheck(T)
@@ -12,3 +40,5 @@ class Solution:
                 stack.pop()
         return stack
         
+"""
+
