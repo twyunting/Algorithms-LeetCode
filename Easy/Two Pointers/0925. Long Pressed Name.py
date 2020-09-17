2b228d40d4940fa15a6587bf687e035f3fff1301
twyunting <yc6705a@american.edu>
2020-09-17 01:26:31 -0400

    pass

--- Easy/Two Pointers/0925. Long Pressed Name.py
+++ Easy/Two Pointers/0925. Long Pressed Name.py
@@ -7,19 +7,12 @@ def isLongPressedName(name, typed):
 	if len(name) > len(typed):
 		return False
 
-	i, j = 0, 0
-	while i < len(name) and j < len(typed):
-		if name[i] == typed[j]:
-			i += 1 # add the length of name
-			j += 1 # add the length of typed
-		elif typed[j] == name[i-1]:
-			j += 1 # add the length of typed
-		else:
+	i = 0
+	for j in range(len(typed)):
+		if i < len(name) and name[i] == typed[j]: #make sure i must < len(name) to save time complexity
+			i += 1
+		elif j == 0 or typed[j] != typed[j-1]: # index 0 cannot minus i
 			return False
+	return i == len(name)
 
-		if i == len(name):
-			return True
-	return False
-
-print(isLonzgPressedName(name = "alex", typed = "aaleex"))
-
+print(isLongPressedName(name = "alex", typed = "aaleex"))
