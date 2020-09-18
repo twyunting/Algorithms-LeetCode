8074fcda05a27a72536d7fc9dd92a227eb3a2114
twyunting <yc6705a@american.edu>
2020-09-18 01:37:46 -0400

    done

--- Easy/String/0014. Longest Common Prefix.py
+++ Easy/String/0014. Longest Common Prefix.py
@@ -7,6 +7,11 @@ def longestCommonPrefix(strs):
 	if not strs:
 		return ""
 
-	return zip(*strs)
+	for i in zip(*strs):
+		if len(set(i)) == 1:
+			res += i[0]
+		else:
+			return res
+	return res
 
 print(longestCommonPrefix(["flower","flow","flight"]))
