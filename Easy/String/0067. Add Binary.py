a6eaf09a6a06df5674eaff3698e9c56dbd4ac346
twyunting <yc6705a@american.edu>
2020-09-29 01:52:34 -0400

    done

--- Easy/String/0067. Add Binary.py
+++ Easy/String/0067. Add Binary.py
@@ -5,7 +5,8 @@ def addBinary(a, b):
 	:rtype: str
 	"""
 	# int("x", base) is change to binary number : 2, 4, 8, 16
-	return "{a:b}".format(int(a, 2) + int(b, 2))
+	return "{:b}".format(int(a, 2) + int(b, 2)) # :b is Binary format
 
-	
-	
+print(addBinary(a = "1010", b = "1011"))
+
+# reference: https://www.w3schools.com/python/ref_string_format.asp
