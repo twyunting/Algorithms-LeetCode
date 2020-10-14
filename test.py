4ab1dfcd34e394103a6b732e81cac38d82b17172
twyunting <yc6705a@american.edu>
2020-10-14 01:37:25 -0400

    test

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
-a = "Let's take LeetCode contest"
-
-
-b = "abcdef"
-
-arr = a.split(" ")
-print(reversed(b))
+# Python code to reverse 
+# a list using slicing 
+  
+ls = ["abc", "def", "ghi"] 
+  
+# list reverse 
+ls = ls[::-1] 
+  
+print('Reversed list elements :') 
+for element in ls: 
+  print(element)
