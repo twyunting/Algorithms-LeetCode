a389449370772c935fdc27f36aeaa176f6b2434b
twyunting <yc6705a@american.edu>
2020-10-16 01:33:48 -0400

    second

--- Easy/String/0680. Valid Palindrome II.py
+++ Easy/String/0680. Valid Palindrome II.py
@@ -3,6 +3,7 @@ def validPalindrome(s):
 	:type s: str
 	:rtype: bool
 	"""
+	
 	i, j = 0, len(s)-1
 
 	while i < j:
@@ -13,12 +14,28 @@ def validPalindrome(s):
 		i += 1
 		j -= 1
 	return True
+	"""
+    def validPalindrome(self, s: str) -> bool:
+        
+        i = 0
+        j = len(s)-1
+        
+        while i < j:
+            if s[i] == s[j]:
+                i += 1
+                j -= 1
+            else:
+                return self.vaildRemoveChr(s, i+1, j) or self.vaildRemoveChr(s, i, j-1)
+        return True
+    
+    def vaildRemoveChr(self, s:str, i:int, j:int) -> bool:
 
-
-
-
-
-
-
-
+        while i < j:
+            if s[i] == s[j]:
+                i += 1
+                j -= 1
+            else:
+                return False
+        return True
+        """
 print(validPalindrome("abca")) # need return True
