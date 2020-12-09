9c7639273e1f3d328f0bf7c08e2afbb7515e2cb3
twyunting <yc6705a@american.edu>
2020-12-09 14:21:34 -0500

    pass

--- Medium/Hash Table/0003. Longest Substring Without Repeating Characters.py
+++ Medium/Hash Table/0003. Longest Substring Without Repeating Characters.py
@@ -4,3 +4,17 @@ def lengthOfLongestSubstring(s):
 	:rtype: int
 	"""
 
+	d = {}
+	start = -1
+	longest = 0
+
+	for i in range(len(s)):
+		if s[i] in d and d[s[i]] > start:
+			start = d[s[i]]
+			d[s[i]] = i
+		else:
+			d[s[i]] = i
+			if i - start > longest:
+				longest = i - start
+	return longest
+print(lengthOfLongestSubstring("abcabcbb"))
