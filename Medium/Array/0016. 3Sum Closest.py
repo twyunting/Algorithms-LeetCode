dbdd7be48bb8edd76bff010e102427dddf034c31
twyunting <yc6705a@american.edu>
2020-12-15 12:17:40 -0500

    done

--- Medium/Array/0016. 3Sum Closest.py
+++ Medium/Array/0016. 3Sum Closest.py
@@ -4,15 +4,26 @@ def threeSumClosest(nums, target):
 	:type target: int
 	:rtype: int
 	"""
-	tmp = []
-	positive = []
-	for i in nums:
-		tmp.append(abs(i - target))
-	return tmp
-	
+	nums.sort()
+	result = nums[0] + nums[1] + nums[len(nums)-1]
 
+	for i in range(len(nums)-2):
+		if i > 0 and nums[i] == nums[i-1]:
+			continue
+		l = i+1
+		r = len(nums)-1
 
-
+		while l < r:
+			val = nums[i] + nums[l] + nums[r]
+			if abs(val-target) < abs(result-target):
+				result = val
+			if val == target:
+				return target
+			elif val < target:
+				l += 1
+			else:
+				r -= 1
+	return result
 
 
 print(threeSumClosest(nums = [-1,2,1,-4], target = 1))
