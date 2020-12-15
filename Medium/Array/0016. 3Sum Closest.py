bc80e81d6c321f447bcda755f3c785eaf3e14d9b
twyunting <yc6705a@american.edu>
2020-12-15 12:29:32 -0500

    solu

--- Medium/Array/0016. 3Sum Closest.py
+++ Medium/Array/0016. 3Sum Closest.py
@@ -26,4 +26,29 @@ def threeSumClosest(nums, target):
 	return result
 
 
+# more readble
+
+"""
+class Solution(object):
+    def threeSumClosest(self, nums, target):       
+        nums.sort()
+        res = sum(nums[:3])
+        
+        for i in range(len(nums)):
+            l, r = i+1, len(nums)-1
+            while l < r:
+                s = sum((nums[i], nums[l], nums[r]))
+                if abs(s-target) < abs(res-target):
+                    res = s
+                if s < target:
+                    l += 1
+                elif s > target:
+                    r -= 1
+                else: # break early 
+                    return res
+        return res
+"""
+
+
+
 print(threeSumClosest(nums = [-1,2,1,-4], target = 1))
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # LeetCode
-* I'm using `python` because it's pretty quick to pick up and it's really convenient to use.
+* I'm using `python` because it's pretty quick to pick up and really convenient to use.
 * The solutions are straightforward for beginners, and I am trying to make my answers to be pythonic and readable
 * Each submission shall have at least one answer.
-* Some codes have been written in `swift`, but just for fun.
+* Some questions have been written in `swift`, but just for fun.
