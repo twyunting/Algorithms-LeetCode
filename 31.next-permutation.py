1a7ba25a58bdb22e9b0b6323774e65d32f161606
twyunting <yc6705a@american.edu>
2020-12-18 13:55:35 -0500

    readme

--- 31.next-permutation.py
+++ 31.next-permutation.py
@@ -15,6 +15,38 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        pivotFirst=-1
+        i=len(nums)-1
+        tmp=nums[-1]
+        while i>=0:
+            if nums[i]>=tmp:
+                tmp=nums[i]
+                i-=1
+            else:
+                pivotFirst=i
+                break
+        #print(pivotFirst)
+        if pivotFirst==-1:
+            nums.reverse()
+            return
+        pivotFirst=i
+        pivotSecond=-1
+        j=len(nums)-1
+        while(j>pivotFirst):
+            if nums[j]>nums[pivotFirst]:
+                pivotSecond=j
+                break
+            j-=1
+
+        #print(pivotFirst,pivotSecond)
+
+        nums[pivotSecond],nums[pivotFirst]=nums[pivotFirst],nums[pivotSecond]
+        l,r = pivotFirst+1, len(nums)-1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l += 1
+            r -= 1
+
         
 # @lc code=end
 
