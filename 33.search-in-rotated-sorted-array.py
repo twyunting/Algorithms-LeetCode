a760a5b300236bf042a0b0cdd7ee566c53152638
twyunting <yc6705a@american.edu>
2020-12-19 10:55:39 -0500

    33

--- 33.search-in-rotated-sorted-array.py
+++ 33.search-in-rotated-sorted-array.py
@@ -17,16 +17,16 @@ class Solution(object):
             mid = (l+r) // 2
             if nums[mid] == target:
                 return mid
-            if target >= nums[0]:
-                if nums[mid] >= nums[0] and target > nums[mid]:
+            elif nums[mid] >= nums[l]:
+                if target >= nums[l] and target < nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            else:
+                if target <= nums[r] and target > nums[mid]:
                     l = mid + 1
                 else:
-                    r = mid - 1
-            else: # target < nums[0]
-                if nums[mid] >= nums[0] or target > nums[mid]:
-                    l = mid + 1
-                else:
-                    r = mid - 1
+                    r = mid - 1    
         return -1
 
             
