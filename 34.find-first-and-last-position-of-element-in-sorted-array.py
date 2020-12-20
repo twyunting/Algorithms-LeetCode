ad43c8295519714dec52deff8eb8bec930a16535
twyunting <yc6705a@american.edu>
2020-12-20 10:48:59 -0500

    39

---
+++ 34.find-first-and-last-position-of-element-in-sorted-array.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=34 lang=python
+#
+# [34] Find First and Last Position of Element in Sorted Array
+#
+
+# @lc code=start
+class Solution(object):
+    def searchRange(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
+        start, end = 0, len(nums)-1
+        while start < end:
+            if 
+# @lc code=end
+
---
+++ 39.combination-sum.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=39 lang=python
+#
+# [39] Combination Sum
+#
+
+# @lc code=start
+class Solution(object):
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+    
+        
+# @lc code=end
+
