7d356054268f7b6a7bc8fd938c9b1cadbd76ce3e
twyunting <yc6705a@american.edu>
2020-12-21 17:43:32 -0500

    done

--- 48.rotate-image.py
+++ 48.rotate-image.py
@@ -11,6 +11,25 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: None Do not return anything, modify matrix in-place instead.
         """
+        # rotate = transpose + reverse
+        column = len(matrix[0])
+
+        # transpose matrix
+        for i in range(column):
+            for j in range(i, column):
+                matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
         
+        # reverse row
+        for i in range(column):
+            matrix[i].reverse()
+        
+
+            
+
+
+
+
+
+
 # @lc code=end
 
