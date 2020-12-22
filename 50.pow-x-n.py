ab49284dfbd08da4562ecb44f1d03c40bc78e32d
twyunting <yc6705a@american.edu>
2020-12-22 15:49:44 -0500

    create

---
+++ 50.pow-x-n.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=50 lang=python
+#
+# [50] Pow(x, n)
+#
+
+# @lc code=start
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        
+        
+# @lc code=end
+
