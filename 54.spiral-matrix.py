cbaa4d38f4c6f445f58db38083d8452e318d5b15
twyunting <yc6705a@american.edu>
2020-12-23 17:19:53 -0500

    go

---
+++ 54.spiral-matrix.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode id=54 lang=python
+#
+# [54] Spiral Matrix
+#
+
+# @lc code=start
+class Solution(object):
+    def spiralOrder(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[int]
+        """
+        row = len(matrix)
+        col = len(matrix[0]
+        )
+
+# @lc code=end
+
--- test.py
+++ test.py
@@ -3,5 +3,10 @@
   
 
 a = [4,5,6,7,0,1,2]
+res = []
 
-print(a[3])
+for i in range(len(a), -1, -1):
+	res.append(i)
+return res
+print(res)
+
