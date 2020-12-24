ac23b01cea913b97ea31ac214cdf2f485dd73cf6
twyunting <yc6705a@american.edu>
2020-12-24 11:41:55 -0500

    54

--- 54.spiral-matrix.py
+++ 54.spiral-matrix.py
@@ -12,8 +12,29 @@ class Solution(object):
         :rtype: List[int]
         """
         row = len(matrix)
-        col = len(matrix[0]
-        )
+        if row == 0 or len(matrix[0]) == 0:
+            return []
+
+        col = len(matrix[0])
+        res = matrix[0]
+
+        if row > 1:
+            for i in range(1, row):
+                res.append(matrix[i][col-1])
+        
+            for j in range(col-2, -1, -1): # we need to include index 0 so use -1
+                res.append(matrix[row-1][j])
+            
+            if col > 1:
+                for k in range(row-2, 0, -1):
+                    res.append(matrix[k][0])
+        
+        inside = []
+        for l in range(1, row-1):
+            tmp = matrix[l][1:-1]
+            inside.append(tmp)
+        
+        return res + self.spiralOrder(inside)
 
 # @lc code=end
 
