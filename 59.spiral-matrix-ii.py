bf4d4d734d843a4059c8447c08fd47f479eb007e
twyunting <yc6705a@american.edu>
2020-12-25 15:07:27 -0500

    create

---
+++ 59.spiral-matrix-ii.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode id=59 lang=python
+#
+# [59] Spiral Matrix II
+#
+
+# @lc code=start
+class Solution(object):
+    def generateMatrix(self, n):
+        """
+        :type n: int
+        :rtype: List[List[int]]
+        """
+        
+# @lc code=end
+
