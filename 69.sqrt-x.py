a3041e9e9bd8b834f9753818014e5dd9004ea5c6
twyunting <yc6705a@american.edu>
2020-12-28 17:35:55 -0500

    cheating 69

---
+++ 69.sqrt-x.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode id=69 lang=python
+#
+# [69] Sqrt(x)
+#
+
+# @lc code=start
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        return int(sqrt(x))
+# @lc code=end
+
