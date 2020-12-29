46b388ebb772c5c0ad2f3cd7a5e23aeefb566b2b
twyunting <yc6705a@american.edu>
2020-12-29 17:10:06 -0500

    push

--- 56.merge-intervals.py
+++ 56.merge-intervals.py
@@ -11,6 +11,18 @@ class Solution(object):
         :type intervals: List[List[int]]
         :rtype: List[List[int]]
         """
-        
+        result = []
+        intervals.sort(key = lambda x:x[0])
+
+        for interval in intervals:
+            # currect does not overlap with the previous
+            if not result or interval[0] > result[-1][1]:
+                result.append(interval)
+            else:
+                result[-1][1] = max(result[-1][1], interval[1])
+        return result
+
+
+
 # @lc code=end
 
