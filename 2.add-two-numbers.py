3203d8eb8be7240fc699aad1a2248f5ed2e73be3
twyunting <yc6705a@american.edu>
2020-12-30 14:16:46 -0500

    2

--- 2.add-two-numbers.py
+++ 2.add-two-numbers.py
@@ -17,6 +17,38 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
+        carry = 0
+        dummy = ListNode(0)
+        p = dummy
+
+        while l1 and l2:
+            p.next = ListNode((l1.val + l2.val + carry) % 10)
+            carry = (l1.val + l2.val + carry) // 10
+            l1 = l1.next
+            l2 = l2.next
+            p = p.next
+        
+        if l1: # if l1 is existing
+            while l1:
+                p.next = ListNode((l1.val + carry) % 10)
+                carry = (l1.val + carry) // 10
+                l1 = l1.next
+                p = p.next
+        
+        if l2: # if l2 is existing
+            while l2:
+                p.next = ListNode((l2.val + carry) % 10)
+                carry = (l2.val + carry) // 10
+                l2 = l2.next
+                p = p.next
+        
+        if carry == 1:
+            p.next = ListNode(1)
+
+        return dummy.next
+
+
+
         
 # @lc code=end
 
--- test.py
+++ test.py
@@ -2,11 +2,7 @@
 # a list using slicing 
   
 
-a = [4,5,6,7,0,1,2]
-res = []
-
-for i in range(len(a), -1, -1):
-	res.append(i)
-return res
-print(res)
+a = 5
+b = 2
+print(a//b)
 
