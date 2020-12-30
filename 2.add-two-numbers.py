8ef30d6e1b9f7ed82b685dd26b9ecdd257d10fe3
twyunting <yc6705a@american.edu>
2020-12-30 15:32:22 -0500

    2

--- 2.add-two-numbers.py
+++ 2.add-two-numbers.py
@@ -17,6 +17,21 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
+        dummy = tail = ListNode(0)
+        s = 0
+
+        while l1 or l2 or s:
+            s = (l1.val if l1 else 0) + (l2.val if l2 else 0) + s
+            tail.next = ListNode(s % 10)
+            tail = tail.next
+            s = s // 10 # carry
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+        return dummy.next 
+
+
+        """
+        #Solution 2
         carry = 0
         dummy = ListNode(0)
         p = dummy
@@ -46,6 +61,7 @@ class Solution(object):
             p.next = ListNode(1)
 
         return dummy.next
+    """
 
 
 
