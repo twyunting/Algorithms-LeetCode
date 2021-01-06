57492c673b05e9c6c06bdfc4d4314c4230088c43
twyunting <yc6705a@american.edu>
2021-01-06 12:28:07 -0500

    pending

---
+++ 221.maximal-square.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=221 lang=python
+#
+# [221] Maximal Square
+#
+
+# @lc code=start
+class Solution(object):
+    def maximalSquare(self, matrix):
+        """
+        :type matrix: List[List[str]]
+        :rtype: int
+        """
+        if matrix == [["0"]]:
+            return 0
+        
+# @lc code=end
+
