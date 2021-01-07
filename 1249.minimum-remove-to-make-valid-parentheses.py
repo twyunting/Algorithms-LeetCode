7cf71a5a3b75105e4413e6e9fa2c3c53e5e8b9a2
twyunting <yc6705a@american.edu>
2021-01-07 11:48:42 -0500

    create 1249

---
+++ 1249.minimum-remove-to-make-valid-parentheses.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode id=1249 lang=python
+#
+# [1249] Minimum Remove to Make Valid Parentheses
+#
+
+# @lc code=start
+class Solution(object):
+    def minRemoveToMakeValid(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        
+# @lc code=end
+
