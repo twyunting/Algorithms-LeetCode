a793ddea545cd3214f11e51cc0e9eb7d12bdaa27
twyunting <yc6705a@american.edu>
2021-01-08 13:24:02 -0500

    push

---
+++ 560.subarray-sum-equals-k.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=560 lang=python
+#
+# [560] Subarray Sum Equals K
+#
+
+# @lc code=start
+class Solution(object):
+    def subarraySum(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        
+# @lc code=end
+
