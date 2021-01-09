250ed7f64925226a5465ee07c78936d0a380c1a1
twyunting <yc6705a@american.edu>
2021-01-09 12:13:38 -0500

    560

--- 560.subarray-sum-equals-k.py
+++ 560.subarray-sum-equals-k.py
@@ -12,6 +12,26 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
+        d = {0:1}
+        res = 0
+        cusum = 0
+
+        for i in nums:
+            cusum += i
+            diff = cusum - k
+            if diff in d:
+                res += d[diff] # value
+            if cusum in d:
+                d[cusum] += 1
+            else:
+                d[cusum] = 1
+        print(d)
+        return res
+
+
+
+
+
         
 # @lc code=end
 
