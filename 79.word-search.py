977a020aebeadd7e141bfc8622da1d2df9737048
twyunting <yc6705a@american.edu>
2021-01-19 11:43:36 -0500

    Create 79.word-search.py

---
+++ 79.word-search.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=79 lang=python
+#
+# [79] Word Search
+#
+
+# @lc code=start
+class Solution(object):
+    def exist(self, board, word):
+        """
+        :type board: List[List[str]]
+        :type word: str
+        :rtype: bool
+        """
+        
+# @lc code=end
+
