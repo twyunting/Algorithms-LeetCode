f60eba56b9ae9f421f029e2d221878bda41e7706
twyunting <yc6705a@american.edu>
2021-01-20 18:24:43 -0500

    Update 155.min-stack.py

--- 155.min-stack.py
+++ 155.min-stack.py
@@ -5,19 +5,59 @@
 #
 
 # @lc code=start
-class MinStack(object):
 
+# Solution 2 : two stack
+class MinStack(object):
     def __init__(self):
         """
         initialize your data structure here.
         """
         self.stack = []
+        self.minStack = []
 
     def push(self, x):
         """
         :type x: int
         :rtype: None
         """
+        self.stack.append(x)
+        
+        if not self.minStack or x <= self.minStack[-1]:
+            self.minStack.append(x)
+
+    def pop(self):
+        """
+        :rtype: None
+        """
+        if self.stack[-1] == self.minStack[-1]:
+            self.minStack.pop()
+        self.stack.pop()
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1]
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return self.minStack[-1]
+        
+    """
+    # solution 1 : one stack
+    def __init__(self):
+        
+        # initialize your data structure here.
+        
+        self.stack = []
+
+    def push(self, x):
+        
+        #:type x: int
+        #:rtype: None
+        
         if not self.stack:
             self.stack.append((x, x))
             return 
@@ -26,23 +66,23 @@ class MinStack(object):
         self.stack.append((x, min(x, current_min)))
 
     def pop(self):
-        """
-        :rtype: None
-        """
+        
+        #:rtype: None
+        
         self.stack.pop()
 
     def top(self):
-        """
-        :rtype: int
-        """
+        
+        #:rtype: int
+        
         return self.stack[-1][0]
 
     def getMin(self):
-        """
-        :rtype: int
-        """
+        
+        #:rtype: int
+        
         return self.stack[-1][1]
-
+    """
 
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
