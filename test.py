8021c3dbe9da06ae31607c8e4d4477dcc109f8e3
twyunting <yc6705a@american.edu>
2021-01-30 10:29:21 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,13 @@
-d = {(x, x + 1): x for x in range(10)}  # Create a dictionary with tuple keys
-t = (5, 6)       # Create a tuple
-print(type(t))
-print(d[t])       
-print(d[(1, 2)])
+import cv2
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import convolve2d as conv2d
+import scipy.sparse as sps
+from PIL import Image
+
+
+a = np.array([[0, 0, 0], [0, 1, 0], [0, 0, 0]])
+print(a)
+b = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])/8
+print(b)
