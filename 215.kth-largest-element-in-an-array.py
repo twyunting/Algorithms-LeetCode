fb7aa7b8d6ffe515e723ee288e9ce05fdc205ff1
twyunting <yc6705a@american.edu>
2021-03-09 16:43:32 -0500

    Create 215.kth-largest-element-in-an-array.py

---
+++ 215.kth-largest-element-in-an-array.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode id=215 lang=python
+#
+# [215] Kth Largest Element in an Array
+#
+
+# @lc code=start
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+    
+        nums = sorted(nums)
+        return nums[-k]
+# @lc code=end
+
